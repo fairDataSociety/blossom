c61079e11d84418ee890b2c4a7c5e791c3eacd4c
Vladan <tomic.vladan.mail@gmail.com>
2022-03-29 15:13:49 +0200

    feat: improve user registration functions

--- src/listeners/message-listeners/auth.listener.ts
+++ src/listeners/message-listeners/auth.listener.ts
@@ -1,5 +1,6 @@
 import BackgroundAction from '../../constants/background-actions.enum'
 import { NULL_ADDRESS } from '../../constants/constants'
+import { Bee } from '../../services/bee.service'
 import { Ether } from '../../services/ether.service'
 
 interface LoginData {
@@ -13,6 +14,7 @@ interface RegisterData {
 }
 
 const etherService = new Ether()
+const bee = new Bee(process.env.SWARM_EXTENSION_ID)
 
 export async function login(username: string, password: string): Promise<void> {
   console.log('Logging in user', username)
@@ -25,7 +27,25 @@ export async function login(username: string, password: string): Promise<void> {
 }
 
 export async function register(username: string, password: string): Promise<void> {
-  await etherService.registerUsername(username)
+  try {
+    await etherService.registerUsername(username)
+
+    // TODO Get batch ID
+    const batchId = 'ddb50acc0d03ae68a00c89e967e319bf73b726005865d76454864804082dd690'
+
+    const signerProvider = etherService.getSignerProvider()
+    await bee.createFeed(
+      username,
+      batchId,
+      password,
+      signerProvider.getPublicKey(),
+      signerProvider.getSigner(),
+    )
+    console.log(`auth.listener: Successfully registered user ${username}`)
+  } catch (error) {
+    console.error(`auth.listeer: Error while trying to register new user ${username}`, error)
+    throw error
+  }
 }
 
 export default function handler(
--- src/services/ens.service.ts
+++ src/services/ens.service.ts
@@ -6,6 +6,10 @@ import SubdomainRegistrarContract from '../contracts/SubdomainRegistrar.json'
 
 const { keccak256, toUtf8Bytes, namehash } = utils
 
+/**
+ * ENS Class
+ * Provides interface for interaction with the ENS smart contracts
+ */
 export class ENS {
   private ensRegistryContract: Contract
   private subdomainRegistrarContract: Contract
@@ -33,13 +37,22 @@ export class ENS {
     )
   }
 
+  /**
+   * Connects the provided signer to the smart contracts
+   * @param signerOrProvider An instance of ethers.js Wallet or any other signer
+   */
   public connect(signerOrProvider: string | providers.Provider | Signer) {
     this.publicResolverContract = this.publicResolverContract.connect(signerOrProvider)
     this.subdomainRegistrarContract = this.subdomainRegistrarContract.connect(signerOrProvider)
     this.ensRegistryContract = this.ensRegistryContract.connect(signerOrProvider)
   }
 
-  public async isUsernameAvailable(username: string): Promise<boolean> {
+  /**
+   * Returns the owner address of the provided username on ENS
+   * @param username ENS username
+   * @returns owner's address
+   */
+  public async getUsernameAddress(username: string): Promise<string> {
     if (!this.isUsernameValid(username)) {
       throw new Error(`ENS: Invalid username ${username}`)
     }
@@ -48,26 +61,49 @@ export class ENS {
 
     const owner = await this.ensRegistryContract.owner(usernameHash)
 
+    return owner
+  }
+
+  /**
+   * Checks whether the provided username is available on ENS
+   * @param username ENS username
+   * @returns True if the username is available
+   */
+  public async isUsernameAvailable(username: string): Promise<boolean> {
+    const owner = await this.getUsernameAddress(username)
+
     return owner === NULL_ADDRESS
   }
 
-  public async createUsername(username: string, address: string): Promise<void> {
-    if (!this.isUsernameValid(username)) {
-      throw new Error(`ENS: Invalid username ${username}`)
-    }
+  /**
+   * Sets the owner of the provided username on ENS
+   * @param username ENS username
+   * @param address Owner address of the username
+   */
+  public registerUsername(username: string, address: string): Promise<void> {
+    return this.subdomainRegistrarContract.register(keccak256(toUtf8Bytes(username)), address)
+  }
 
-    await this.subdomainRegistrarContract.register(keccak256(toUtf8Bytes(username)), address)
-
-    await this.ensRegistryContract.setResolver(
+  /**
+   * Sets resolver for the provided ENS username
+   * @param username ENS username
+   */
+  public setUsernameResolver(username: string): Promise<void> {
+    return this.ensRegistryContract.setResolver(
       this.hashSubdomain(username),
       this.publicResolverContract.address,
     )
   }
 
+  /**
+   * Sets user's public key to the user's ENS entry
+   * @param username ENS username
+   * @param address Owner of the username
+   * @param publicKey Public key that will be added to ENS
+   */
   public setUsernamePublicKey(username: string, address: string, publicKey: string): Promise<void> {
     const usernameHash = this.hashSubdomain(username)
-    const publicKeyX = publicKey.substring(0, 66)
-    const publicKeyY = '0x' + publicKey.substring(66, 130)
+    const [publicKeyX, publicKeyY] = this.splitPublicKey(publicKey)
     const content = '0x0000000000000000000000000000000000000000000000000000000000000000'
     const name = 'subdomain-hidden'
 
@@ -82,6 +118,11 @@ export class ENS {
     )
   }
 
+  /**
+   * Fetches all ENS data related to the provided username
+   * @param username ENS username
+   * @returns
+   */
   public getUserData(username: string): Promise<unknown> {
     return this.publicResolverContract.getAll(this.hashSubdomain(username))
   }
@@ -93,5 +134,12 @@ export class ENS {
 
   private hashSubdomain(subdomain: string): string {
     return namehash(`${subdomain}.${this.domain}`)
+  }
+
+  private splitPublicKey(publicKey: string): [string, string] {
+    const publicKeyX = publicKey.substring(0, 66)
+    const publicKeyY = '0x' + publicKey.substring(66, 130)
+
+    return [publicKeyX, publicKeyY]
   }
 }
