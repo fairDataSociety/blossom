2f5d47d621cf814e0a0a4aa57ebb65529bf153e2
Vladan <tomic.vladan.mail@gmail.com>
2022-05-19 13:49:20 +0200

    feat: add dapp message listener

---
+++ src/constants/dapp-actions.enum.ts
@@ -0,0 +1,3 @@
+import BackgroundAction from './background-actions.enum'
+
+export const DAPP_ACTIONS: string[] = [BackgroundAction.LOGIN, BackgroundAction.REGISTER]
--- src/content/index.ts
+++ src/content/index.ts
@@ -1,5 +1,47 @@
-console.log('Content script...')
+import { DAPP_ACTIONS } from '../constants/dapp-actions.enum'
+import { BLOSSOM_API_EVENT, BLOSSOM_API_RESPONSE_EVENT } from '../constants/events'
+import { MessageResponse, sendMessage } from '../messaging/scripts.messaging'
+import { isNumber, isBackgroundAction } from '../utils/asserts'
 
-chrome.runtime.sendMessage('abc', (response) => {
-  console.log('FDP Content Script: ', response)
+function createResponseEvent(requestId: number, response: MessageResponse<unknown>) {
+  const event = new CustomEvent(BLOSSOM_API_RESPONSE_EVENT, {
+    detail: {
+      ...response,
+      requestId,
+    },
+  })
+
+  document.dispatchEvent(event)
+}
+
+document.addEventListener(BLOSSOM_API_EVENT, async (event: any) => {
+  const { detail } = event
+
+  if (!detail) {
+    console.warn('Blossom: Received invalid request from the page')
+
+    return
+  }
+
+  const { requestId, action, data } = detail
+
+  if (!isNumber(requestId)) {
+    console.warn('Blossom: Invalid request ID')
+
+    return
+  }
+
+  if (!isBackgroundAction(action) || !DAPP_ACTIONS.includes(action)) {
+    console.warn('Blossom: Invalid action')
+
+    return
+  }
+
+  try {
+    const response = await sendMessage<unknown, MessageResponse<unknown>>(action, data)
+
+    createResponseEvent(requestId, response)
+  } catch (error) {
+    createResponseEvent(requestId, { error })
+  }
 })
