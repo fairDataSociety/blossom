8351e15e3f9a6e9003c28425a6fc7223b7d0f1dc
Vladan <tomic.vladan.mail@gmail.com>
2022-05-27 17:02:13 +0200

    test: fix test utils

--- library/test/utils/extension.ts
+++ library/test/utils/extension.ts
@@ -2,6 +2,7 @@ import { Browser } from 'puppeteer'
 
 export async function getExtensionId(browser: Browser): Promise<string> {
   const page = await browser.newPage()
+  await page.goto('http://localhost:9000', { waitUntil: 'load' })
   const targets = await browser.targets()
   const extensionTarget = targets.find(target => target.type() === 'service_worker')
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
