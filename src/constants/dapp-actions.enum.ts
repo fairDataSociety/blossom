96aed421a1d7efe6d0a89d7cde9c8981b5893524
Vladan <tomic.vladan.mail@gmail.com>
2022-06-23 15:06:46 +0200

    feat: separate dapp and e2e actions

--- src/constants/dapp-actions.enum.ts
+++ src/constants/dapp-actions.enum.ts
@@ -1,7 +1,5 @@
 import BackgroundAction from './background-actions.enum'
 
-export const DAPP_ACTIONS: string[] = [
-  BackgroundAction.LOGIN,
-  BackgroundAction.REGISTER,
-  BackgroundAction.ECHO,
-]
+export const DAPP_ACTIONS: string[] = [BackgroundAction.ECHO]
+
+export const E2E_ACTIONS: string[] = [BackgroundAction.ECHO]
--- src/listeners/e2e.listener.ts
+++ src/listeners/e2e.listener.ts
@@ -1,4 +1,4 @@
-import { DAPP_ACTIONS } from '../constants/dapp-actions.enum'
+import { E2E_ACTIONS } from '../constants/dapp-actions.enum'
 import { Message } from '../messaging/scripts.messaging'
 import { isBackgroundAction } from '../utils/asserts'
 import { messageHandler } from './message-listeners'
@@ -6,7 +6,7 @@ import { messageHandler } from './message-listeners'
 chrome.runtime.onMessageExternal.addListener((request: Message<unknown>, sender, sendResponse) => {
   const { action } = request || {}
 
-  if (!isBackgroundAction(action) || !DAPP_ACTIONS.includes(action)) {
+  if (!isBackgroundAction(action) || !E2E_ACTIONS.includes(action)) {
     sendResponse({ error: `Blossom: invalid action received from the extension with ID ${sender.id}` })
 
     return true
