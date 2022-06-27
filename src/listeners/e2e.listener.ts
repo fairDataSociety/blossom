0f85acf54da557ef137deaea90036662ae99143c
Vladan <tomic.vladan.mail@gmail.com>
2022-06-27 18:22:50 +0200

    fix: improve message security

--- src/listeners/e2e.listener.ts
+++ src/listeners/e2e.listener.ts
@@ -1,17 +1,7 @@
-import { E2E_ACTIONS } from '../constants/dapp-actions.enum'
 import { Message } from '../messaging/scripts.messaging'
-import { isBackgroundAction } from '../utils/asserts'
 import { messageHandler } from './message-listeners'
 
 chrome.runtime.onMessageExternal.addListener((request: Message<unknown>, sender, sendResponse) => {
-  const { action } = request || {}
-
-  if (!isBackgroundAction(action) || !E2E_ACTIONS.includes(action)) {
-    sendResponse({ error: `Blossom: invalid action received from the extension with ID ${sender.id}` })
-
-    return false
-  }
-
   messageHandler(request, sender, sendResponse)
 
   return true
