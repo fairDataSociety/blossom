e2abcd564bea6c3979f0197478c966ac9cbf2cba
Vladan <tomic.vladan.mail@gmail.com>
2022-07-06 12:17:10 +0200

    fix: fix registration types

--- src/messaging/message.asserts.ts
+++ src/messaging/message.asserts.ts
@@ -1,5 +1,12 @@
 import { Account } from '../model/general.types'
-import { LoginData, RegisterData, UsernameCheckData } from '../model/internal-messages.model'
+import {
+  LoginData,
+  RegisterData,
+  RegisterDataBase,
+  RegisterDataMnemonic,
+  RegisterDataPrivateKey,
+  UsernameCheckData,
+} from '../model/internal-messages.model'
 
 export function isLoginData(data: unknown): data is LoginData {
   const loginData = (data || {}) as LoginData
@@ -8,9 +15,25 @@ export function isLoginData(data: unknown): data is LoginData {
 }
 
 export function isRegisterData(data: unknown): data is RegisterData {
-  const registerData = (data || {}) as RegisterData
+  return Boolean(isRegisterDataMnemonic(data) || isRegisterDataPrivateKey(data))
+}
 
-  return Boolean(registerData.username && registerData.password)
+export function isRegisterDataMnemonic(data: unknown): data is RegisterDataMnemonic {
+  const registerData = (data || {}) as RegisterDataMnemonic
+
+  return Boolean(isRegisterDataBase(data) && registerData.mnemonic)
+}
+
+export function isRegisterDataPrivateKey(data: unknown): data is RegisterDataPrivateKey {
+  const registerData = (data || {}) as RegisterDataPrivateKey
+
+  return Boolean(isRegisterDataBase(data) && registerData.privateKey)
+}
+
+export function isRegisterDataBase(data: unknown): data is RegisterDataBase {
+  const registerData = (data || {}) as RegisterDataBase
+
+  return Boolean(registerData.username && registerData.password && registerData.network)
 }
 
 export function isAccount(data: unknown): data is Account {
