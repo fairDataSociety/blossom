0837400a19ff8cee8e090541b786142814923c15
tomicvladan <tomic.vladan.mail@gmail.com>
2022-07-28 15:47:52 +0200

    ci: fix tests

--- test/config/setup.ts
+++ test/config/setup.ts
@@ -1,27 +1,22 @@
-import puppeteer, { Browser } from 'puppeteer'
+import puppeteer from 'puppeteer'
 import path from 'path'
 import { getExtensionId } from '../utils/extension.util'
-import { createPostageBatch } from '../utils/bee'
 
 const EXTENSION_ROOT = path.join(__dirname, '..', '..')
 const EXTENSION_DIST = path.join(EXTENSION_ROOT, 'dist')
 const SWARM_EXTENSION_PATH = path.join(EXTENSION_ROOT, 'swarm-extension/dist')
 
 export default async () => {
-  const [browser] = await Promise.all([
-    puppeteer.launch({
-      executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container in github CI environment
-      headless: false,
-      args: [
-        `--no-sandbox`, //Required for this to work in github CI environment
-        `--disable-extensions-except=${EXTENSION_DIST},${SWARM_EXTENSION_PATH}`,
-        `--load-extension=${EXTENSION_DIST},${SWARM_EXTENSION_PATH}`,
-      ],
-    }),
-    process.env.CREATE_POSTAGE_BATCH === 'true'
-      ? createPostageBatch(process.env.BEE_DEBUG_API_URL || 'http://localhost:1635')
-      : Promise.resolve(),
-  ])
+  const browser = await puppeteer.launch({
+    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container in github CI environment
+    headless: false,
+    args: [
+      `--no-sandbox`, //Required for this to work in github CI environment
+      `--disable-extensions-except=${EXTENSION_DIST},${SWARM_EXTENSION_PATH}`,
+      `--load-extension=${EXTENSION_DIST},${SWARM_EXTENSION_PATH}`,
+    ],
+  })
+
   global.__BROWSER__ = browser
   global.__BLOSSOM_ID__ = await getExtensionId('Blossom')
   global.__SWARM_ID__ = await getExtensionId('Swarm')
--- test/utils/bee.ts
+++
@@ -1,7 +0,0 @@
-import http from 'http'
-
-export function createPostageBatch(beeUrl): Promise<void> {
-  return new Promise((resolve, reject) => {
-    http.get(`${beeUrl}/stamps/10000000/18`, () => resolve()).on('error', (error) => reject(error))
-  })
-}
