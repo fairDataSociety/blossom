6f3c7d62e4bfa6efc16afa74111d5d3a938c85a2
tomicvladan <tomic.vladan.mail@gmail.com>
2022-08-10 11:45:03 +0200

    ci: fix ci

--- test/ci/run-tests-ci.sh
+++ test/ci/run-tests-ci.sh
@@ -1,6 +1,4 @@
 
-APP_DIR=$(pwd)
-
 docker build -t puppeteer -f "test/ci/Dockerfile" .
 
-docker run --rm --net="host" -v "$APP_DIR":"/app"  puppeteer:latest  npm run test
+/usr/bin/docker run --net="host" --workdir /github/workspace --rm -e BEE_API_URL -e BEE_DEBUG_API_URL -e BEE_PEER_API_URL -e BEE_PEER_DEBUG_API_URL -e CI -e INPUT_ARGS -e HOME -e GITHUB_JOB -e GITHUB_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_REPOSITORY_OWNER -e GITHUB_RUN_ID -e GITHUB_RUN_NUMBER -e GITHUB_RETENTION_DAYS -e GITHUB_RUN_ATTEMPT -e GITHUB_ACTOR -e GITHUB_TRIGGERING_ACTOR -e GITHUB_WORKFLOW -e GITHUB_HEAD_REF -e GITHUB_BASE_REF -e GITHUB_EVENT_NAME -e GITHUB_SERVER_URL -e GITHUB_API_URL -e GITHUB_GRAPHQL_URL -e GITHUB_REF_NAME -e GITHUB_REF_PROTECTED -e GITHUB_REF_TYPE -e GITHUB_WORKSPACE -e GITHUB_ACTION -e GITHUB_EVENT_PATH -e GITHUB_ACTION_REPOSITORY -e GITHUB_ACTION_REF -e GITHUB_PATH -e GITHUB_ENV -e GITHUB_STEP_SUMMARY -e RUNNER_OS -e RUNNER_ARCH -e RUNNER_NAME -e RUNNER_TOOL_CACHE -e RUNNER_TEMP -e RUNNER_WORKSPACE -e ACTIONS_RUNTIME_URL -e ACTIONS_RUNTIME_TOKEN -e ACTIONS_CACHE_URL -e GITHUB_ACTIONS=true -v "/var/run/docker.sock":"/var/run/docker.sock" -v "/home/runner/work/_temp/_github_home":"/github/home" -v "/home/runner/work/_temp/_github_workflow":"/github/workflow" -v "/home/runner/work/_temp/_runner_file_commands":"/github/file_commands" -v "/home/runner/work/blossom/blossom":"/github/workspace" puppeteer:latest npm run test:ci
