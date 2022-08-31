0ad27a40a020b43d1a4b36049808381f00f3a48c
tomicvladan <tomic.vladan.mail@gmail.com>
2022-08-31 15:33:05 +0200

    fix: fetch postage batches (#69)
    * fix: fetch postage batches
    
    * fix: fix postage batch fetch function

---
+++ src/utils/bee.ts
@@ -0,0 +1,24 @@
+import { BeeDebug } from '@ethersphere/bee-js'
+
+/**
+ * Gets postage batch ID from already created batches
+ *
+ * @param beeDebugUrl URL of Bee debug API
+ */
+export async function getBatchId(beeDebugUrl: string): Promise<string> {
+  const beeDebug = new BeeDebug(beeDebugUrl)
+
+  const batches = await beeDebug.getAllPostageBatch()
+
+  if (batches.length === 0) {
+    throw new Error('Postage batch not exists')
+  }
+
+  const { batchID, usable } = batches.pop()
+
+  if (!batchID || !usable) {
+    throw new Error('Incorrect batch id found')
+  }
+
+  return batchID
+}
