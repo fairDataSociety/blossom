bdd65f183c843fa7dcfa04fa1b5f11d48313e6b0
tomicvladan <tomic.vladan.mail@gmail.com>
2022-10-20 14:53:18 +0200

    fix: live-reload fixes

--- src/live-reload.ts
+++ src/live-reload.ts
@@ -6,12 +6,14 @@ function setupLiveReloadConnections(address: string): void {
     connection = new WebSocket(address)
   }
   connection.onmessage = () => {
+    console.log('Service worker script changed. Reloading extension...')
     chrome.runtime.reload()
+  }
+  connection.onerror = () => {
+    console.error(`Couldn't open WebSocket connection to ${RELOAD_ADDRESS}`)
   }
 }
 
-chrome.management.getSelf((self) => {
-  if (self.installType === 'development') {
-    setupLiveReloadConnections(RELOAD_ADDRESS)
-  }
-})
+if (process.env.ENVIRONMENT === 'development') {
+  setupLiveReloadConnections(RELOAD_ADDRESS)
+}
