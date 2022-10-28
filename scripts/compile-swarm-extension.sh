29de909ccb0d9fab9fb1132765d2549428986b77
tomicvladan <tomic.vladan.mail@gmail.com>
2022-10-28 11:05:26 +0200

    test: fix swarm extension script

--- scripts/compile-swarm-extension.sh
+++ scripts/compile-swarm-extension.sh
@@ -17,8 +17,9 @@ cd swarm-extension
 
 npm ci
 
-if [[ $CI = "true" ]] then
-  sed -i 's/127.0.0.1/172.18.0.1/g' swarm-extension/src/background/constants/addresses.ts
+if [[ $CI = "true" ]]
+then
+  sed -i 's/127.0.0.1/172.18.0.1/g' src/background/constants/addresses.ts
 fi
 
 npm run compile
