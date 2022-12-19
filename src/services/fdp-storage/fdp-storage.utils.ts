c5fc5cc8dfd849bef35ae273e8e7a4a4be0a9fb6
tomicvladan <tomic.vladan.mail@gmail.com>
2022-12-19 16:28:35 +0100

    feat: add support for subdomain based apps

--- src/services/fdp-storage/fdp-storage.utils.ts
+++ src/services/fdp-storage/fdp-storage.utils.ts
@@ -1,19 +1,48 @@
 import { assertBeeUrl } from '../../messaging/message.asserts'
 import { DappId } from '../../model/general.types'
 
-export function dappUrlToId(url: string, beeUrl: string): DappId {
-  assertBeeUrl(beeUrl)
+function constructSubdomainRegex(beeUrl: string): RegExp {
+  const [protocol, domain] = beeUrl.split('://')
 
+  // extracts dApp ENS name from a swarm subdomain link (e.g http://ENS.swarm.localhost:1633...)
+  return new RegExp(
+    `${protocol}://(.+).swarm.${domain.endsWith('/') ? domain.substring(0, domain.length - 1) : domain}.*`,
+  )
+}
+
+function constructBzzRegex(beeUrl: string): RegExp {
   const bzzUrl = beeUrl + (beeUrl.endsWith('/') ? '' : '/') + 'bzz/'
 
   // extracts dApp ENS name from a bzz link (e.g http://localhost:1633/bzz/ENS/...)
-  const result = new RegExp(`${bzzUrl}([^/]+).*`).exec(url)
+  return new RegExp(`${bzzUrl}([^/]+).*`)
+}
 
-  if (!result || !result[1]) {
-    throw new Error('Invalid dApp URL')
+function extractDappIdFromRegex(url: string, regex: RegExp): string | null {
+  const result = regex.exec(url)
+
+  if (result && result[1]) {
+    return result[1]
   }
 
-  return result[1]
+  return null
+}
+
+export function dappUrlToId(url: string, beeUrl: string): DappId {
+  assertBeeUrl(beeUrl)
+
+  let dappId = extractDappIdFromRegex(url, constructBzzRegex(beeUrl))
+
+  if (dappId) {
+    return dappId
+  }
+
+  dappId = extractDappIdFromRegex(url, constructSubdomainRegex(beeUrl))
+
+  if (dappId) {
+    return dappId
+  }
+
+  throw new Error('Invalid dApp URL')
 }
 
 export function dappIdToPodName(dappId: DappId): string {
