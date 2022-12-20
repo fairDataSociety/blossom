b2d46dea9b1186d170661b9e926a59fc81f308fd
tomicvladan <tomic.vladan.mail@gmail.com>
2022-12-20 16:01:39 +0100

    fix: swarm api (#99)
    * fix: swarm api
    
    * feat: add support for subdomain based apps
    
    * chore: update readme
    
    * test: dapp id extraction

--- scripts/compile-swarm-extension.sh
+++ scripts/compile-swarm-extension.sh
@@ -19,7 +19,7 @@ npm ci
 
 if [[ $CI = "true" ]]
 then
-  sed -i 's/127.0.0.1/172.18.0.1/g' src/background/constants/addresses.ts
+  sed -i 's/localhost/172.18.0.1/g' src/background/constants/addresses.ts
 fi
 
 npm run compile
