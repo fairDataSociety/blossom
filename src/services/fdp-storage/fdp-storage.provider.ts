32e5d307e287b45f0e73d1b86477e69654e6eeb8
Vladan <tomic.vladan.mail@gmail.com>
2023-08-21 09:20:40 +0200

    feat: configurable bee url (#157)

--- src/services/fdp-storage/fdp-storage.provider.ts
+++ src/services/fdp-storage/fdp-storage.provider.ts
@@ -55,7 +55,7 @@ export abstract class FdpStorageProvider {
     beeApiUrl: string
     beeDebugApiUrl: string
   }> {
-    let beeApiUrl = 'http://localhost:1633',
+    let beeApiUrl = swarm.swarmUrl || 'http://localhost:1633',
       beeDebugApiUrl = 'http://localhost:1635'
 
     if (process.env.CI_TESTS === 'true') {
@@ -63,6 +63,10 @@ export abstract class FdpStorageProvider {
         beeApiUrl: 'http://172.18.0.1:1633',
         beeDebugApiUrl: 'http://172.18.0.1:1635',
       }
+    }
+
+    if (!swarm.extensionEnabled) {
+      return { beeApiUrl, beeDebugApiUrl: null }
     }
 
     try {
